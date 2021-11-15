2f5d3b0797914500cf788db6cae2488c501019b8
oabecerrago <becerra.orlando@yandex.com>
2021-11-15 10:05:04 -0500

    ajuste link mongoDB

--- src/datasources/mongodb.datasource.ts
+++ src/datasources/mongodb.datasource.ts
@@ -4,7 +4,7 @@ import {juggler} from '@loopback/repository';
 const config = {
   name: 'mongodb',
   connector: 'mongodb',
-  url: 'mongodb+srv://dbUsuario:progweb2022@oabecerrago.9glnc.mongodb.net/MoviCar?retryWrites=true&w=majority',
+  url: 'mongodb+srv://movicar:progweb@cluster0.nsrln.mongodb.net/MoviCar?retryWrites=true&w=majority',
   host: '',
   port: 0,
   user: '',
