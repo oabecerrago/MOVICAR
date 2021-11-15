0670982dc31c7866bcd2de7e6f0116cd1f2373c5
oabecerrago <becerra.orlando@yandex.com>
2021-11-15 10:07:54 -0500

    --

--- src/datasources/mongodb.datasource.ts
+++ src/datasources/mongodb.datasource.ts
@@ -4,7 +4,7 @@ import {juggler} from '@loopback/repository';
 const config = {
   name: 'mongodb',
   connector: 'mongodb',
-  url: 'mongodb+srv://movicar:progweb@cluster0.nsrln.mongodb.net/MoviCar?retryWrites=true&w=majority',
+  url: 'mongodb+srv://movicar:progweb2022@cluster0.nsrln.mongodb.net/MoviCar?retryWrites=true&w=majority',
   host: '',
   port: 0,
   user: '',
